cec538944c497bf105debe5bbbbbdea59f004ce7
777freezy <109428496+777freezy@users.noreply.github.com>
2023-02-12 22:27:27 -0300

    Update index.js

--- chat/index.js
+++ chat/index.js
@@ -80,7 +80,7 @@ const commands = (client, message) => {
                  * nosso próprio número e sim para 
                  * a pessoa ou grupo para o qual eu enviei
                  */
-                client.sendText(message.from === process.env.BOT_NUMBER ? message.to : message.from, response)
+                client.sendText(message.from === process.env.PHONE_NUMBER ? message.to : message.from, response)
             })
             break;
 
@@ -88,7 +88,7 @@ const commands = (client, message) => {
             const imgDescription = message.text.substring(message.text.indexOf(" "));
             getDalleResponse(imgDescription, message).then((imgUrl) => {
                 client.sendImage(
-                    message.from === process.env.BOT_NUMBER ? message.to : message.from,
+                    message.from === process.env.PHONE_NUMBER ? message.to : message.from,
                     imgUrl,
                     imgDescription,
                     'Imagem  IA FREEZY'
