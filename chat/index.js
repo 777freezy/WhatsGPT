a1457e14882540dc6e11419a7b2e3f222fe99770
777freezy <109428496+777freezy@users.noreply.github.com>
2023-03-11 00:06:24 -0300

    Update index.js

--- chat/index.js
+++ chat/index.js
@@ -1,23 +1,17 @@
 import { create } from 'venom-bot'
 import * as dotenv from 'dotenv'
 import { Configuration, OpenAIApi } from "openai"
-import puppeteer from "puppeteer"
 
 dotenv.config()
 
-const browser = await puppeteer.launch({
-    headless: true
-});
-
 create({
     session: 'Chat-GPT',
-    multidevice: true,
-    useChrome: true,
+    multidevice: true
 })
     .then((client) => start(client))
     .catch((erro) => {
-    console.log(erro)
-})
+        console.log(erro);
+    });
 
 const configuration = new Configuration({
     organization: process.env.ORGANIZATION_ID,
@@ -28,10 +22,10 @@ const openai = new OpenAIApi(configuration);
 
 const getDavinciResponse = async (clientText) => {
     const options = {
-        model: "text-davinci-003", 
-        prompt: clientText, 
-        temperature: 1, 
-        max_tokens: 1000 
+    model: "text-davinci-003", 
+    prompt: clientText,
+    temperature: 0.5,
+    max_tokens: 100,
     }
 
     try {
@@ -73,7 +67,14 @@ const commands = (client, message) => {
         case iaCommands.davinci3:
             const question = message.text.substring(message.text.indexOf(" "));
             getDavinciResponse(question).then((response) => {
-                client.sendText(message.from === process.env.PHONE_NUMBER ? message.to : message.from, response)
+                /*
+                 * Faremos uma validação no message.from
+                 * para caso a gente envie um comando
+                 * a response não seja enviada para
+                 * nosso próprio número e sim para 
+                 * a pessoa ou grupo para o qual eu enviei
+                 */
+                client.sendText(message.from === process.env.BOT_NUMBER ? message.to : message.from, response)
             })
             break;
 
@@ -81,7 +82,7 @@ const commands = (client, message) => {
             const imgDescription = message.text.substring(message.text.indexOf(" "));
             getDalleResponse(imgDescription, message).then((imgUrl) => {
                 client.sendImage(
-                    message.from === process.env.PHONE_NUMBER ? message.to : message.from,
+                    message.from === process.env.BOT_NUMBER ? message.to : message.from,
                     imgUrl,
                     imgDescription,
                     'Imagem  IA FREEZY'
